79a0217f3e836212738bac9178dbab0cc00f6608
elleon003 <noelle@ygitc.com>
2024-08-03 03:14:05 -0400

    Finalized user profile

--- account/views.py
+++ account/views.py
@@ -2,7 +2,13 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm, UserRegistrationForm
+from .forms import (
+    LoginForm, 
+    UserRegistrationForm,
+    ProfileEditForm,
+    UserEditForm
+    )
+from .models import Profile
 
 def user_login(request):
     if request.method =='POST':
@@ -39,7 +45,7 @@ def dashboard(request):
     )
 
 def register(request):
-    if request.methoc =='POST':
+    if request.method =='POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
             # Create a new user object but don't save it yet
@@ -50,15 +56,45 @@ def register(request):
             )
             # Save the User object
             new_user.save()
+            # Create the user profile
+            Profile.objects.create(user=new_user)
             return render(
                 request,
                 'account/register_done.html',
                 {'new_user': new_user}
             )
-        else:
-            user_form = UserRegistrationForm()
-        return render(
-            request,
-            'account/register.html',
-            {'user_form': user_form}
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request,
+        'account/register.html',
+        {'user_form': user_form}
+    )
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST
         )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form
+        }
+    )
--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
@@ -18,3 +20,9 @@ urlpatterns = [
         name='django.contrib.sitemaps.views.sitemap'
     )
 ]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+    
